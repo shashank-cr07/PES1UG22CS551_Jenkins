8d52f600dbdc380231391ca534b109c302772640
shashank-cr07 <151113029+shashank-cr07@users.noreply.github.com>
2025-03-18 15:55:42 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout<<"Bye friends"\n;
+  cout<<"Bye friends\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
